d3774670e0521df8cc2e3d646627549afd8fa937
Kim Wistbacka <kim.wistbacka@gmail.com>
2018-01-15 12:59:26 +0200

    Remove AccessorMutator from AbstractModel

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -4,11 +4,7 @@ namespace Qm;
 use \Qm\Interfaces\PersistenceInterface;
 use \Qm\Traits\PersistenceTrait;
 
-use \Qm\Interfaces\AccessorMutatorInterface;
-use \Qm\Traits\AccessorMutatorTrait;
-
-abstract class AbstractModel implements PersistenceInterface, AccessorMutatorInterface
+abstract class AbstractModel implements PersistenceInterface
 {
     use PersistenceTrait;
-    use AccessorMutatorTrait;
 }
